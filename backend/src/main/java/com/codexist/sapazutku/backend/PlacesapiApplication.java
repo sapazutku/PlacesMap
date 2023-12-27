f19d6cfa3e97556b35f8df3132730010c5ea9048
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2023-12-27 20:59:25 +0300

    first commit

---
+++ backend/src/main/java/com/codexist/sapazutku/backend/PlacesapiApplication.java
@@ -0,0 +1,13 @@
+package com.codexist.sapazutku.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codexist/sapazutku/backend/PlacesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexist.sapazutku.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
